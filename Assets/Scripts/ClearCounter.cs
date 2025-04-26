8351032a11ffdf9e276686c5bed04868a71ba88b
dorje253 <tdorje070@gmail.com>
2025-04-26 23:39:27 +0545

    changing the parent
    change parent of object if t pressed with secondclearObject and clear the pervious object

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -4,12 +4,47 @@ public class ClearCounter : MonoBehaviour
 {
    [SerializeField] private KitchenObjectSO kitchenObjectSo;
    [SerializeField] private Transform counterTopPoint;
+   [SerializeField] private ClearCounter secondClearCounter;
+   [SerializeField] private bool testing;
+
+   private KitchenObject kitchenObject;
+
+   private void Update(){
+    if(testing && Input.GetKeyDown(KeyCode.T)){
+        if(kitchenObject != null){
+            kitchenObject.SetclearCounter(secondClearCounter);
+        }
+    }
+   }
 
 public void Interact(){
-    Debug.Log("Interact");
-    Transform kitchenObjectTransfrom = Instantiate(kitchenObjectSo.prefab, counterTopPoint);
-    kitchenObjectTransfrom.localPosition = Vector3.zero;
+    if(kitchenObject == null){
+        Transform kitchenObjectTransfrom = Instantiate(kitchenObjectSo.prefab, counterTopPoint);
+        kitchenObjectTransfrom.GetComponent<KitchenObject>().SetclearCounter(this);
+        kitchenObjectTransfrom.localPosition = Vector3.zero;
+    }else{
+        Debug.Log(kitchenObject.GetClearCounter());
+    }
+    
+}
 
-    Debug.Log(kitchenObjectTransfrom.GetComponent<KitchenObject>().GetKitchenObjectSO().objectName);
+public Transform GetKitchenObjectFollowTransform(){
+    return counterTopPoint;
+}
+
+public void SetKitchenObject(KitchenObject kitchenObject){
+    this.kitchenObject = kitchenObject;
+}
+
+public KitchenObject GetKitchenObject(){
+    return kitchenObject;
+}
+
+public void ClearKitchenObject(){
+    kitchenObject = null;
+}
+
+public bool HasKitchenObject(){
+    return kitchenObject != null;
 }
 }
--- Assets/Scripts/KitchenObject.cs
+++ Assets/Scripts/KitchenObject.cs
@@ -3,8 +3,30 @@ using UnityEngine;
 public class KitchenObject : MonoBehaviour
 {
     [SerializeField] private KitchenObjectSO  kitchenObjectSO;
+    private  ClearCounter clearCounter;
+
 
     public KitchenObjectSO GetKitchenObjectSO(){
         return kitchenObjectSO;
+    }
+
+    public void SetclearCounter(ClearCounter clearCounter){
+        if(this.clearCounter != null){
+            this.clearCounter.ClearKitchenObject();
+        }
+        this.clearCounter = clearCounter;
+
+        if(clearCounter.HasKitchenObject()){
+            Debug.LogError("Counter alrady has a kitchenObject !");
+        }
+        
+        clearCounter.SetKitchenObject(this);
+
+        transform.parent = clearCounter.GetKitchenObjectFollowTransform();
+        transform.localPosition = Vector3.zero;
+    }
+
+    public ClearCounter GetClearCounter(){
+        return clearCounter;
     }
 }
