896fe4638adc20d6261ea3bf2df2e1d0df224dfc
dorje253 <tdorje070@gmail.com>
2025-04-26 22:50:30 +0545

    Spawn kitchen object and identity
    Instantiate object in counter top point and using scriptable object using class kitchenObjectSO and get identify from kitchenObjecr script which attach to  kitchen prefabs

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -2,7 +2,14 @@ using UnityEngine;
 
 public class ClearCounter : MonoBehaviour
 {
+   [SerializeField] private KitchenObjectSO kitchenObjectSo;
+   [SerializeField] private Transform counterTopPoint;
+
 public void Interact(){
-    // Debug.Log("Interact");
+    Debug.Log("Interact");
+    Transform kitchenObjectTransfrom = Instantiate(kitchenObjectSo.prefab, counterTopPoint);
+    kitchenObjectTransfrom.localPosition = Vector3.zero;
+
+    Debug.Log(kitchenObjectTransfrom.GetComponent<KitchenObject>().GetKitchenObjectSO().objectName);
 }
 }
