0771405f2c84f30ee1fbe1d9b6c2419be8d9041c
dorje253 <tdorje070@gmail.com>
2025-05-01 23:27:43 +0545

    Add PlatesCounter
    add PlatesCounter inheritance base counter and using PlaterCounterVisual to spawn Plate visual and destory
    
    creating event to spawn the plate visual according to spawnPlatesTimer, platesSpawnAmount

---
+++ Assets/Scripts/Counter/PlatesCounter.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using System;
+public class PlatesCounter : BaseCounter
+{
+    public event EventHandler OnPlateSpawned;
+    public event EventHandler OnPlateRemoved;
+
+    [SerializeField] private KitchenObjectSO plateKitchenObjectSO;
+   private float spawnPlatesTimer;
+   private float spawnPlatesTimerMax = 4.0f;
+   private int platesSpawnAmount;
+   private int platesSpawnAmountMax = 4;
+
+
+   private void Update(){
+    spawnPlatesTimer += Time.deltaTime;
+    if(spawnPlatesTimer > spawnPlatesTimerMax){
+        spawnPlatesTimer = 0f;
+        if(platesSpawnAmount < platesSpawnAmountMax){
+            platesSpawnAmount++;
+            OnPlateSpawned?.Invoke(this, EventArgs.Empty);
+        }
+        // KitchenObject.SpawnKitchenObject(plateKitchenObjectSO, this);
+    }
+   }
+
+   
+    public override void Interact(Player player){
+        if(!player.HasKitchenObject()){
+            // Player is Empty Handed
+            if(platesSpawnAmount>0){
+                // there is at least one plate here
+                platesSpawnAmount--;
+                KitchenObject.SpawnKitchenObject(plateKitchenObjectSO, player);
+                
+                OnPlateRemoved?.Invoke(this, EventArgs.Empty);
+            }
+        }
+    }
+}
