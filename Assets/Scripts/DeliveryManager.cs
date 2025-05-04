1825c33d6e43873ba085e914afd514566396dfbc
dorje253 <tdorje070@gmail.com>
2025-05-04 10:31:40 +0545

    add Delivery Manager UI
    add DeliveryManagerUI and spwan the deliverycontainer according to Recipe list
    
    container containe recipename and icons template
    
    icon template spawn according to   recipeSO.kitchenObjectSOList

--- Assets/Scripts/DeliveryManager.cs
+++ Assets/Scripts/DeliveryManager.cs
@@ -1,7 +1,12 @@
 using UnityEngine;
 using System.Collections.Generic;
+using System;
+
 public class DeliveryManager : MonoBehaviour
 {
+    public event EventHandler OnRecipeSpawned;
+    public event EventHandler OnRecipeCompleted;
+
     public static DeliveryManager Instance {  get; private set; }
 
     [SerializeField] private RecipeListSO recipeListSO;
@@ -25,14 +30,14 @@ public class DeliveryManager : MonoBehaviour
             spawnRecipeTimer = spawnRecipeTimerMax;
             if(waitingRecipeSOList.Count < waitingRecipeMax)
             {
-                RecipeSO waitingRecipeSO = recipeListSO.recipeSOList[Random.Range(0, recipeListSO.recipeSOList.Count)];
-                Debug.Log(waitingRecipeSO.recipeName);
+                RecipeSO waitingRecipeSO = recipeListSO.recipeSOList[UnityEngine.Random.Range(0, recipeListSO.recipeSOList.Count)];
                 waitingRecipeSOList.Add(waitingRecipeSO);
+                OnRecipeSpawned?.Invoke(this, EventArgs.Empty);
             }
-            
+
         }
     }
-    
+
 
     public void DeliverRecipe(PlateKitchenObject plateKitchenObject)
     {
@@ -67,9 +72,9 @@ public class DeliveryManager : MonoBehaviour
 
                 if (plateContentsMatchesRecipe)
                 {
-                    // Player delivered the correct recipe!
-                    Debug.Log("player delivered the correct recipe!");
+                    // Player delivered the correct recipe!           
                     waitingRecipeSOList.RemoveAt(i);
+                    OnRecipeCompleted?.Invoke(this, EventArgs.Empty);
                     return;
                 }
             }
@@ -77,6 +82,11 @@ public class DeliveryManager : MonoBehaviour
 
         // No matches found!
         // Player did not deliver a correct recipe
-        Debug.Log("player did not delivered the correct recipe ");
+       
+    }
+
+    public List<RecipeSO> GetWaitingRecipeSOList()
+    {
+        return waitingRecipeSOList;
     }
 }
