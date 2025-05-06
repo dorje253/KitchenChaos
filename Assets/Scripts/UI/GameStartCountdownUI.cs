f65d4c31e3ef2247925b1028afe2f837027985cc
dorje253 <tdorje070@gmail.com>
2025-05-06 19:52:49 +0545

    add PopSound and animation in CountdownUI
    add Popsound from sound Manager and creating the CountDownUI animation and using animation in GameStartCountdownUI

--- Assets/Scripts/UI/GameStartCountdownUI.cs
+++ Assets/Scripts/UI/GameStartCountdownUI.cs
@@ -5,6 +5,16 @@ public class GameStartCountdownUI : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI countdownText;
 
+    private const string NUMBER_POPUP = "NumberPopup";
+
+    private Animator animator;
+    private int previousCountdownNumber;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
     private void Start()
     {
         KitchenGameManager.Instance.OnStateChanged += KitchenGameManage_OnStateChanged;
@@ -25,7 +35,14 @@ public class GameStartCountdownUI : MonoBehaviour
 
     private void Update()
     {
-        countdownText.text = Mathf.Ceil(KitchenGameManager.Instance.GetCountdownToStartTimer()).ToString();
+        int countdownNumber = Mathf.CeilToInt(KitchenGameManager.Instance.GetCountdownToStartTimer());
+        countdownText.text = countdownNumber.ToString();
+        if(previousCountdownNumber != countdownNumber)
+        {
+            previousCountdownNumber = countdownNumber;
+            animator.SetTrigger(NUMBER_POPUP);
+            SoundManager.Instance.PlayCountdownSound();
+        }
     }
 
     private void Show()
