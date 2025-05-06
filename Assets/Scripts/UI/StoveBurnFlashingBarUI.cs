dfdbd4221efc7a53ee20b2973df710605a893051
dorje253 <tdorje070@gmail.com>
2025-05-06 22:36:47 +0545

    add Flashing animation in progress bar
    add  flashing red animation in progess bar in stove counter  and using the OnProgressChange  event to called the flashing animation

---
+++ Assets/Scripts/UI/StoveBurnFlashingBarUI.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class StoveBurnFlashingBarUI : MonoBehaviour
+{
+    private const string IS_FLASHING = "IsFlashing";
+
+    [SerializeField] private StoveCounter stoveCounter;
+    private Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void Start()
+    {
+        stoveCounter.OnProgressChange += StoveCounter_OnProgressChange;
+        animator.SetBool(IS_FLASHING, false);
+    }
+
+    private void StoveCounter_OnProgressChange(object sender, IHasProgress.OnProgressChangeEventArgs e)
+    {
+        float burnShowProgessAmount = .5f;
+        bool show = stoveCounter.IsFried() && e.progressNormalized >= burnShowProgessAmount;
+        animator.SetBool(IS_FLASHING, show);
+    }
+
+    
+}
