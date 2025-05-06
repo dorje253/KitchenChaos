088168df3eaf6aa4e1bf9ce6f03728df29aeb9e1
dorje253 <tdorje070@gmail.com>
2025-05-06 14:54:25 +0545

    Add rebuid for joystick  or gamepad and select feature  for button
    add new binding for joystick for action interact, interact alternative and pause and disable the pause UI when option UI is active and show pause UI when close button  is press in option UI

--- Assets/Scripts/UI/GamePauseUI.cs
+++ Assets/Scripts/UI/GamePauseUI.cs
@@ -23,7 +23,8 @@ public class GamePauseUI : MonoBehaviour
 
         optionsButton.onClick.AddListener(() =>
         {
-            OptionsUI.Instance.Show();
+            Hide();
+            OptionsUI.Instance.Show(Show);
         });
     }
 
@@ -49,6 +50,7 @@ public class GamePauseUI : MonoBehaviour
     private void Show()
     {
         gameObject.SetActive(true);
+        resumeButton.Select();
     }
 
     private void Hide()
--- Assets/Scripts/UI/OptionsUI.cs
+++ Assets/Scripts/UI/OptionsUI.cs
@@ -1,6 +1,8 @@
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using System;
+
 
 public class OptionsUI : MonoBehaviour
 {
@@ -18,6 +20,10 @@ public class OptionsUI : MonoBehaviour
     [SerializeField] private Button interactAltButton;
     [SerializeField] private Button pauseButton;
 
+    [SerializeField] private Button gamepadInteractButton;
+    [SerializeField] private Button gamepadInteractAltButton;
+    [SerializeField] private Button gamepadPauseButton;
+
     [SerializeField] private TextMeshProUGUI soundEffectsText;
     [SerializeField] private TextMeshProUGUI musicText;
 
@@ -29,8 +35,16 @@ public class OptionsUI : MonoBehaviour
     [SerializeField] private TextMeshProUGUI interactText;
     [SerializeField] private TextMeshProUGUI interactAltText;
     [SerializeField] private TextMeshProUGUI pauseText;
+
+    [SerializeField] private TextMeshProUGUI gamepadInteractText;
+    [SerializeField] private TextMeshProUGUI gamepadInteractAltText;
+    [SerializeField] private TextMeshProUGUI gamepadPauseText;
+
     [SerializeField] private Transform presstoRebindKeyTransform;
-   
+
+    private Action onCloseButtonAction;
+
+
 
     private void Awake()
     {
@@ -49,7 +63,9 @@ public class OptionsUI : MonoBehaviour
 
         CloseButton.onClick.AddListener(() =>
         {
+            
             Hide();
+            onCloseButtonAction();
         });
 
         moveUpButton.onClick.AddListener(() =>{RebindBinding(GameInput.Binding.Move_Up); });
@@ -65,6 +81,12 @@ public class OptionsUI : MonoBehaviour
         interactAltButton.onClick.AddListener(() =>{RebindBinding(GameInput.Binding.InteractAlternate); });
 
         pauseButton.onClick.AddListener(() =>{RebindBinding(GameInput.Binding.Pause); });
+
+        gamepadInteractButton.onClick.AddListener(() => { RebindBinding(GameInput.Binding.Gamepad_Interact); });
+
+        gamepadInteractAltButton.onClick.AddListener(() => { RebindBinding(GameInput.Binding.Gamepad_InteractAlternate); });
+
+        gamepadPauseButton.onClick.AddListener(() => { RebindBinding(GameInput.Binding.Gamepad_Pause); });
     }
 
     private void Start()
@@ -93,13 +115,18 @@ public class OptionsUI : MonoBehaviour
         interactText.text = GameInput.Instance.GetBindingText(GameInput.Binding.Interact);
         interactAltText.text = GameInput.Instance.GetBindingText(GameInput.Binding.InteractAlternate);
         pauseText.text = GameInput.Instance.GetBindingText(GameInput.Binding.Pause);
-        
+        gamepadInteractText.text = GameInput.Instance.GetBindingText(GameInput.Binding.Gamepad_Interact);
+        gamepadInteractAltText.text = GameInput.Instance.GetBindingText(GameInput.Binding.Gamepad_InteractAlternate);
+        gamepadPauseText.text = GameInput.Instance.GetBindingText(GameInput.Binding.Gamepad_Pause);
+
 
     }
 
-    public void Show()
+    public void Show(Action onCloseButtonAction)
     {
+        this.onCloseButtonAction = onCloseButtonAction;
         gameObject.SetActive(true);
+        soundEffectsButton.Select();
     }
 
     public void Hide()
