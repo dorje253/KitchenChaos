2535fd8bbb5b636085490b6e3a25f1eba26eec45
dorje253 <tdorje070@gmail.com>
2025-05-19 22:39:56 +0545

    add player visual and ready UI in Character Select scene
    add player visual when player join and show Ready UI when player is ready.
    
    to shown when player is join with visual player with help playerindex. playerindex is used to check if player index in exist in KitchenGameMultiplayer playerDataNetworkList or not
    
    playerDataNetworkList add using NetworkManager OnClientConnectedCallback methid  or event in PlayerData struct
    
    to check player is ready for not through OnReadyChanged event. this event is inoke when playeid  is true in playerReadyDictionary and SetPlayerReadyClientRpc to client and invoke it and used IsPlayerReady to  return bool to CharacterSelectPlayer and used OnReadyChanged  event to check playerdata  or clientid  from KitchenGameMultiplayer  is true or not

---
+++ Assets/Scripts/CharacterSelectPlayer.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+using System.Collections;
+
+public class CharacterSelectPlayer : MonoBehaviour
+{
+    [SerializeField] private int playerIndex;
+    [SerializeField] private GameObject readyGameObject;
+
+    private void Start()
+    {
+        KitchenGameMultiplayer.Instance.OnPlayerDataNetworkListChanged += KitchenGameMultiplayer_OnPlayerDataNetworkListChanged;
+        CharacterSelectReady.Instance.OnReadyChanged += CharacterSelectReady_OnReadyChanged;
+        UpdatePlayer();
+    }
+
+    private void CharacterSelectReady_OnReadyChanged(object sender, System.EventArgs e)
+    {
+        UpdatePlayer();
+    }
+
+    private void KitchenGameMultiplayer_OnPlayerDataNetworkListChanged(object sender, System.EventArgs e)
+    {
+        UpdatePlayer();
+    }
+
+    private void UpdatePlayer()
+    {
+        if (KitchenGameMultiplayer.Instance.IsPlayerIndexConnected(playerIndex))
+        {
+            Show();
+            PlayerData playerData = KitchenGameMultiplayer.Instance.GetPlayerDataFromPlayerIndex(playerIndex);
+
+            readyGameObject.SetActive(CharacterSelectReady.Instance.IsPlayerReady(playerData.clientId));
+
+        }
+        else
+        {
+            Hide();
+        }
+    }
+
+    private void Hide()
+    {
+        gameObject.SetActive(false);
+    }
+
+    private void Show()
+    {
+        gameObject.SetActive(true);
+    }
+}
